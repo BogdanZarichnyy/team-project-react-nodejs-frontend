221a64cc307a3826d354074e89425134fb5c9db5
feninTolia <fenintolja@gmail.com>
2022-12-29 18:05:56 +0200

    layout and header setup

---
+++ src/Components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from 'react-router-dom';
+
+import Header from '../Header/Header';
+import style from './SharedLayout.module.css';
+
+const SharedLayout = () => {
+  return (
+    <div className={style.layoutContainer}>
+      <Header />
+
+      <div className={style.outlets}>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default SharedLayout;
