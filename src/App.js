a38f9eb5d3290f282dd6f53e6be3449c009cefdf
OstapenkoIvan <ostapenko.ivan@gmail.com>
2023-01-02 20:45:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ export const App = () => {
           <Route path="register" element={<RegisterPage />} />
           <Route path="friends" element={<OurFriendsPages />} />
           <Route path="news" element={<NewsList />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="profile" element={<Profile />} />
         </Route>
 
         <Route path="*" element={<span>Not Found</span>} />
