8b46f8b2c5e7effe49851eed017e41dbf951043b
feninTolia <fenintolja@gmail.com>
2023-01-06 11:17:16 +0200

    added currentUserFetch on page load, added check is user logged in on auth buttons group

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,11 +1,21 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Outlet } from 'react-router-dom';
-import CookiesPopup from '../CookiesPopup';
+import { getUserFetch } from '../../store/user/userSlice';
 
+import CookiesPopup from '../CookiesPopup';
 import Header from '../Header';
 
 import style from './SharedLayout.module.scss';
 
 const SharedLayout = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUserFetch());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   const isSeenCookieBar = document.cookie
     .split('; ')
     .includes('cookieBarSeen=true');
