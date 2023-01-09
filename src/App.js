ba6ccbf238c9129fec0e5108ce11bd1c1df75715
feninTolia <fenintolja@gmail.com>
2023-01-09 15:38:58 +0200

    edited routes, and rendering

--- src/App.js
+++ src/App.js
@@ -5,44 +5,99 @@ import PublicRoute from './routes/PublicRoute';
 import PrivateRoute from './routes/PrivateRoute';
 
 import SharedLayout from './components/SharedLayout/SharedLayout';
+import Loader from './components/LoaderV1';
 import Home from './pages/Home';
-import LoginPage from './pages/LoginPage/LoginPage';
-import Profile from './pages/Profile';
-import RegisterPage from './pages/RegisterPage';
+// import LoginPage from './pages/LoginPage/LoginPage';
+// import Profile from './pages/Profile';
+// import RegisterPage from './pages/RegisterPage';
 
 import './App.css';
 
 const OurFriendsPages = lazy(() =>
   import('./pages/OurFriendsPages/OurFriendsPages')
 );
-
 const NoticesPage = lazy(() => import('./pages/NoticesPage'));
-
 const NewsList = lazy(() => import('./pages/News/NewsList'));
+const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage'));
+const RegisterPage = lazy(() => import('./pages/RegisterPage'));
+const Profile = lazy(() => import('./pages/Profile'));
 
 export const App = () => {
   return (
-    <Suspense fallback={<span>Loading</span>}>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Home />} />
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route
+          index
+          element={
+            <Suspense fallback={<Loader />}>
+              <Home />
+            </Suspense>
+          }
+        />
 
-          <Route element={<PublicRoute />}>
-            <Route path="login" element={<LoginPage />} />
-            <Route path="register" element={<RegisterPage />} />
-          </Route>
-
-          <Route element={<PrivateRoute />}>
-            <Route path="user" element={<Profile />} />
-          </Route>
-
-          <Route path="friends" element={<OurFriendsPages />} />
-          <Route path="news" element={<NewsList />} />
-          <Route path="notices" element={<NoticesPage />} />
+        <Route
+          element={
+            <Suspense fallback={<Loader />}>
+              <PublicRoute />
+            </Suspense>
+          }
+        >
+          <Route
+            path="login"
+            element={
+              <Suspense fallback={<Loader />}>
+                <LoginPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="register"
+            element={
+              <Suspense fallback={<Loader />}>
+                <RegisterPage />
+              </Suspense>
+            }
+          />
         </Route>
 
-        <Route path="*" element={<span>Not Found</span>} />
-      </Routes>
-    </Suspense>
+        <Route element={<PrivateRoute />}>
+          <Route
+            path="user"
+            element={
+              <Suspense fallback={<Loader />}>
+                <Profile />
+              </Suspense>
+            }
+          />
+        </Route>
+
+        <Route
+          path="friends"
+          element={
+            <Suspense fallback={<Loader />}>
+              <OurFriendsPages />
+            </Suspense>
+          }
+        />
+        <Route
+          path="news"
+          element={
+            <Suspense fallback={<Loader />}>
+              <NewsList />
+            </Suspense>
+          }
+        />
+        <Route
+          path="notices"
+          element={
+            <Suspense fallback={<Loader />}>
+              <NoticesPage />
+            </Suspense>
+          }
+        />
+      </Route>
+
+      <Route path="*" element={<span>Not Found</span>} />
+    </Routes>
   );
 };
