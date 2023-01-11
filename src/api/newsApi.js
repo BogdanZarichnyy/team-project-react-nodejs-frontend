b325f32a2648220dac972eb878ba7eb5390eda44
OstapenkoIvan <ostapenko.ivan@gmail.com>
2023-01-11 15:06:06 +0200

    update

--- src/api/newsApi.js
+++ src/api/newsApi.js
@@ -1,6 +1,8 @@
 import { newsApi, apiErrorHandler } from './main';
 
-export const getNews = async () => {
-  const { data } = await newsApi.get().catch(apiErrorHandler);
+export const getNews = async (searchData = '') => {
+  const { data } = await newsApi
+    .get(`query=${searchData}`)
+    .catch(apiErrorHandler);
   return data;
 };
