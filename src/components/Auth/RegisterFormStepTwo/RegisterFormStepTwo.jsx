15d70f34fa520a94afd6777622841a2290c9f261
Kopchuk Maksym <kopchuk.maksym@gmail.com>
2023-01-12 17:30:10 +0200

    test commit

--- src/components/Auth/RegisterFormStepTwo/RegisterFormStepTwo.jsx
+++ src/components/Auth/RegisterFormStepTwo/RegisterFormStepTwo.jsx
@@ -2,38 +2,39 @@ import React, { useEffect, useState } from 'react';
 import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
 import Select from 'react-select';
+import citiesArray from '../../../constants/citiesArray';
 
 import InputBase from '../../InputBase/InputBase';
 import ButtonBase from '../../ButtonBase/ButtonBase';
 import ErrorText from '../../ErrorText';
 
 import s from '../Auth.module.scss';
-
+console.log(citiesArray);
 const RegisterFormStepTwo = ({ onNext, formik }) => {
-  const [cities, setCities] = useState([]);
+  // const [cities, setCities] = useState([]);
   const [filteredCities, setFilteredCities] = useState([]);
 
-  useEffect(() => {
-    fetch(
-      'https://test-team-project-react-nodejs-production.up.railway.app/api/cities_of_ukraine'
-    )
-      .then(res => res.json())
-      .then(
-        result => {
-          setCities(result);
-        },
-        error => {
-          setCities(error);
-        }
-      );
-  }, []);
+  // useEffect(() => {
+  //   fetch(
+  //     'https://test-team-project-react-nodejs-production.up.railway.app/api/cities_of_ukraine'
+  //   )
+  //     .then(res => res.json())
+  //     .then(
+  //       result => {
+  //         setCities(result);
+  //       },
+  //       error => {
+  //         setCities(error);
+  //       }
+  //     );
+  // }, []);
 
   const searchCity = value => {
     if (!value.length) {
       setFilteredCities([]);
       return;
     }
-    const filteredCities = cities.reduce((acc, el) => {
+    const filteredCities = citiesArray.reduce((acc, el) => {
       if (el.city.toUpperCase().startsWith(value.toUpperCase())) {
         acc.push({
           value: `${el.city}, ${el.admin_name}`,
