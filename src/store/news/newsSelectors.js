a2aa9f7800d08a2b1e92fb3b21feec45ada0535d
feninTolia <fenintolja@gmail.com>
2023-01-14 13:44:16 +0200

    setup

--- src/store/news/newsSelectors.js
+++ src/store/news/newsSelectors.js
@@ -1,3 +1,13 @@
 export const getNewsSelector = state => state.news.newsData;
 export const getLoadingdNewsSelector = state => state.news.isLoading;
 export const getErrorNewsSelector = state => state.news.error;
+
+// getNewsSelector;
+
+export const sortedNewsSelector = state => {
+  const news = getNewsSelector(state);
+  const sortedNews = [...scores].sort((a, b) => b - a);
+  console.log(sortedNews); // [92, 74, 61, 56, 35, 19]
+
+  return sortedNews;
+};
